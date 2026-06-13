d75df64d39c89c141ca045b969fa56a5f16468ec
Rizki Laksana Putra <120088587+RizkiLaksanaPutra@users.noreply.github.com>
2026-06-13 20:17:29 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,23 +4,6 @@ const Footer = () => {
     return (
         <footer className="relative z-10 flex w-5/6 flex-col gap-8 py-8 lg:w-3/4">
             <div className="flex w-full flex-wrap gap-8 sm:flex-nowrap">
-                <div className="w-full flex-grow lg:w-3/5">
-                    <h2 className="text-xl font-bold text-accent-300">NAVIGATION</h2>
-                    <ul className="mt-2 space-y-1 font-satoshi">
-                        <li>
-                            <Link href="#home">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="#about">About</Link>
-                        </li>
-                        <li>
-                            <Link href="#experiences">Experiences</Link>
-                        </li>
-                        <li>
-                            <Link href="#projects">Projects</Link>
-                        </li>
-                    </ul>
-                </div>
                 <div className="w-full flex-grow lg:w-1/5">
                     <h2 className="text-xl font-bold text-accent-300">SOCIALS</h2>
                     <ul className="mt-2 space-y-1 font-satoshi">
